3bca0c394877bdf63f70f1519883fb7231b89e53
Richard <rvanlaak@gmail.com>
2016-03-11 17:54:30 +0100

    PSR encoding, line ends

--- lib/DetectLanguage/Client.php
+++ lib/DetectLanguage/Client.php
@@ -4,166 +4,171 @@ namespace DetectLanguage;
 
 class Client
 {
-  /**
-   * Request engine.
-   *
-   * @var string Request engine (curl or stream).
-   */
-  public static $requestEngine = 'curl';
+    /**
+     * Request engine.
+     *
+     * @var string Request engine (curl or stream).
+     */
+    public static $requestEngine = 'curl';
 
-  /**
-   * Request timeout.
-   *
-   * @var int
-   */
-  public static $requestTimeout = 60;
+    /**
+     * Request timeout.
+     *
+     * @var int
+     */
+    public static $requestTimeout = 60;
 
-  /**
-   * Connect timeout.
-   *
-   * @var int
-   */
-  public static $connectTimeout = 10;
+    /**
+     * Connect timeout.
+     *
+     * @var int
+     */
+    public static $connectTimeout = 10;
 
-  /**
-   * Perform a request
-   *
-   * @param string $method Method name
-   * @param array $params The parameters to use for the POST body
-   *
-   * @return array
-   */
-  public static function request($method, $params) {
-    $url = self::getUrl($method);
+    /**
+     * Perform a request
+     *
+     * @param string $method Method name
+     * @param array $params The parameters to use for the POST body
+     *
+     * @return array
+     */
+    public static function request($method, $params)
+    {
+        $url = self::getUrl($method);
 
-    $params['key'] = DetectLanguage::$apiKey;
+        $params['key'] = DetectLanguage::$apiKey;
+        $request_method = self::getRequestMethodName();
+        $response_body = self::$request_method($url, $params);
+        $response = json_decode($response_body);
 
-    $request_method = self::getRequestMethodName();
+        if (isset($response->error))
+            throw new Error($response->error->message);
 
-    $response_body = self::$request_method($url, $params);
-
-    $response = json_decode($response_body);
-
-    if (isset($response->error))
-      throw new Error($response->error->message);
-
-    return $response;
-  }
-
-  /**
-   * Get request method name.
-   *
-   * @return string
-   */
-  protected static function getRequestMethodName() {
-  	$request_engine = self::$requestEngine;
-
-  	if ($request_engine == 'curl' && !function_exists('curl_init')) {
-  		trigger_error("DetectLanguage::Client - CURL not found, switching to stream");
-  		$request_engine = self::$requestEngine = 'stream';
-  	}
-
-    switch ($request_engine) {
-      case 'curl':
-        return 'requestCurl';
-      
-      case 'stream':
-        return 'requestStream';
-
-      default:
-        throw new Error("Invalid request engine: " . $request_engine);
-    }
-  }
-
-  /**
-   * Perform request using native PHP streams
-   *
-   * @param string $url Request URL
-   * @param array $params The parameters to use for the POST body
-   *
-   * @return string Response body
-   */
-  protected static function requestStream($url, $params) {
-    $postdata = http_build_query($params);
-
-    $opts = array('http' =>
-      array(
-        'method'            => 'POST',
-        'header'            => implode("\n", self::getHeaders()),
-        'content'           => $postdata,
-        'timeout'           => self::$requestTimeout
-      )
-    );
-
-    $context = stream_context_create($opts);
-
-    return file_get_contents($url, false, $context);
-  }
-
-  /**
-   * Perform request using CURL extension.
-   *
-   * @param string $url Request URL
-   * @param array $params The parameters to use for the POST body
-   *
-   * @return string Response body
-   */
-  protected static function requestCurl($url, $params) {
-    $ch = curl_init();
-
-    $options = array(
-      CURLOPT_URL             => $url,
-      CURLOPT_HTTPHEADER      => self::getHeaders(),
-      CURLOPT_POSTFIELDS      => http_build_query($params),
-      CURLOPT_CONNECTTIMEOUT  => self::$connectTimeout,
-      CURLOPT_TIMEOUT         => self::$requestTimeout,
-      CURLOPT_USERAGENT       => self::getUserAgent(),
-      CURLOPT_RETURNTRANSFER  => true
-      );
-
-    curl_setopt_array($ch, $options);
-
-    $result = curl_exec($ch);
-
-    if ($result === false) {
-      $e = new Error(curl_error($ch));
-      curl_close($ch);
-      throw $e;
+        return $response;
     }
 
-    curl_close($ch);
+    /**
+     * Get request method name.
+     *
+     * @return string
+     */
+    protected static function getRequestMethodName()
+    {
+        $request_engine = self::$requestEngine;
 
-    return $result;
-  }
+        if ($request_engine == 'curl' && !function_exists('curl_init')) {
+            trigger_error("DetectLanguage::Client - CURL not found, switching to stream");
+            $request_engine = self::$requestEngine = 'stream';
+        }
 
-  /**
-   * Build URL for given method
-   *
-   * @param string $method Method name
-   * @return string
-   */
-  protected static function getUrl($method) {
-    return self::getProtocol() . '://' . DetectLanguage::$host . '/' . DetectLanguage::$apiVersion . '/' . $method;
-  }
+        switch ($request_engine) {
+            case 'curl':
+                return 'requestCurl';
 
-  protected static function getProtocol() {
-    return DetectLanguage::$secure ? 'https' : 'http';
-  }
+            case 'stream':
+                return 'requestStream';
 
-  /**
-   * Build request headers.
-   *
-   * @return string
-   */
-  protected static function getHeaders() {
-    return array(
-      "Content-Type: application/x-www-form-urlencoded",
-      "Accept-Encoding: gzip, deflate",
-      "User-Agent: " . self::getUserAgent()
-    );
-  }
+            default:
+                throw new Error("Invalid request engine: " . $request_engine);
+        }
+    }
 
-  protected static function getUserAgent() {
-	  return 'detectlanguage-php-' . DetectLanguage::VERSION;
-  }
+    /**
+     * Perform request using native PHP streams
+     *
+     * @param string $url Request URL
+     * @param array $params The parameters to use for the POST body
+     *
+     * @return string Response body
+     */
+    protected static function requestStream($url, $params)
+    {
+        $postdata = http_build_query($params);
+
+        $opts = array('http' =>
+            array(
+                'method' => 'POST',
+                'header' => implode("\n", self::getHeaders()),
+                'content' => $postdata,
+                'timeout' => self::$requestTimeout
+            )
+        );
+
+        $context = stream_context_create($opts);
+
+        return file_get_contents($url, false, $context);
+    }
+
+    /**
+     * Perform request using CURL extension.
+     *
+     * @param string $url Request URL
+     * @param array $params The parameters to use for the POST body
+     *
+     * @return string Response body
+     */
+    protected static function requestCurl($url, $params)
+    {
+        $ch = curl_init();
+
+        $options = array(
+            CURLOPT_URL => $url,
+            CURLOPT_HTTPHEADER => self::getHeaders(),
+            CURLOPT_POSTFIELDS => http_build_query($params),
+            CURLOPT_CONNECTTIMEOUT => self::$connectTimeout,
+            CURLOPT_TIMEOUT => self::$requestTimeout,
+            CURLOPT_USERAGENT => self::getUserAgent(),
+            CURLOPT_RETURNTRANSFER => true
+        );
+
+        curl_setopt_array($ch, $options);
+
+        $result = curl_exec($ch);
+
+        if ($result === false) {
+            $e = new Error(curl_error($ch));
+            curl_close($ch);
+            throw $e;
+        }
+
+        curl_close($ch);
+
+        return $result;
+    }
+
+    /**
+     * Build URL for given method
+     *
+     * @param string $method Method name
+     * @return string
+     */
+    protected static function getUrl($method)
+    {
+        return self::getProtocol() . '://' . DetectLanguage::$host . '/' . DetectLanguage::$apiVersion . '/' . $method;
+    }
+
+    protected static function getProtocol()
+    {
+        return DetectLanguage::$secure ? 'https' : 'http';
+    }
+
+    /**
+     * Build request headers.
+     *
+     * @return string
+     */
+    protected static function getHeaders()
+    {
+        return array(
+            "Content-Type: application/x-www-form-urlencoded",
+            "Accept-Encoding: gzip, deflate",
+            "User-Agent: " . self::getUserAgent()
+        );
+    }
+
+    protected static function getUserAgent()
+    {
+        return 'detectlanguage-php-' . DetectLanguage::VERSION;
+    }
 }
--- lib/DetectLanguage/DetectLanguage.php
+++ lib/DetectLanguage/DetectLanguage.php
@@ -4,98 +4,103 @@ namespace DetectLanguage;
 
 
 if (!function_exists('json_decode')) {
-  throw new Error('DetectLanguage needs the JSON PHP extension.');
+    throw new Error('DetectLanguage needs the JSON PHP extension.');
 }
 
 class DetectLanguage
 {
-  /**
-   * The API key.
-   *
-   * @static
-   * @var string
-   */
-  public static $apiKey;
+    /**
+     * The API key.
+     *
+     * @static
+     * @var string
+     */
+    public static $apiKey;
 
-  /**
-   * API host.
-   *
-   * @static
-   * @var string
-   */
-  public static $host = 'ws.detectlanguage.com';
+    /**
+     * API host.
+     *
+     * @static
+     * @var string
+     */
+    public static $host = 'ws.detectlanguage.com';
 
-  /**
-   * API version.
-   *
-   * @static
-   * @var string
-   */
-  public static $apiVersion = '0.2';
+    /**
+     * API version.
+     *
+     * @static
+     * @var string
+     */
+    public static $apiVersion = '0.2';
 
-  /**
-   * Enable secure mode (SSL).
-   *
-   * @static
-   * @var boolean
-   */
-  public static $secure;
+    /**
+     * Enable secure mode (SSL).
+     *
+     * @static
+     * @var boolean
+     */
+    public static $secure;
 
-  /**
-   * API Client Version.
-   */
-  const VERSION = '2.0.1';
+    /**
+     * API Client Version.
+     */
+    const VERSION = '2.0.1';
 
-  /**
-   * Set API key
-   *
-   * @static
-   * @var string
-   */
-  public static function setApiKey($apiKey) {
-    self::$apiKey = $apiKey;
-  }
+    /**
+     * Set API key
+     *
+     * @static
+     * @var string
+     */
+    public static function setApiKey($apiKey)
+    {
+        self::$apiKey = $apiKey;
+    }
 
-  /**
-   * Set secure mode
-   *
-   * @static
-   * @var boolean
-   */
-  public static function setSecure($secure) {
-    self::$secure = $secure;
-  }
+    /**
+     * Set secure mode
+     *
+     * @static
+     * @var boolean
+     */
+    public static function setSecure($secure)
+    {
+        self::$secure = $secure;
+    }
 
-  /**
-   * Detect text language.
-   *
-   * @static
-   * @param string @text The text for language detection
-   * @return array detected languages information
-   */
-  public static function detect($text) {
-    $result = Client::request('detect', array('q' => $text));
+    /**
+     * Detect text language.
+     *
+     * @static
+     * @param string @text The text for language detection
+     * @return array detected languages information
+     */
+    public static function detect($text)
+    {
+        $result = Client::request('detect', array('q' => $text));
 
-    return $result->data->detections;
-  }
+        return $result->data->detections;
+    }
 
-  /**
-   * Simple detection. If you need just the language code.
-   *
-   * @static
-   * @param string @text The text for language detection
-   * @return string detected language code
-   */
-  public static function simpleDetect($text) {
-    $detections = self::detect($text);
+    /**
+     * Simple detection. If you need just the language code.
+     *
+     * @static
+     * @param string @text The text for language detection
+     * @return string detected language code
+     */
+    public static function simpleDetect($text)
+    {
+        $detections = self::detect($text);
 
-    if (count($detections) > 0)
-      return $detections[0]->language;
-    else
-      return null;
-  }
+        if (count($detections) > 0)
+            return $detections[0]->language;
+        else
+            return null;
+    }
 
-  public static function getStatus() {
-    return Client::request('user/status', array());
-  }
+    public static function getStatus()
+    {
+        return Client::request('user/status', array());
+    }
 }
